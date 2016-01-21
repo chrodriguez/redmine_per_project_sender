c07eb06969b3e793ab39251c49e9dd52cfec9c1f
Anton Nechay <anton.nechay90@gmail.com>
2016-01-21 11:29:52 +0300

    Fix version number

--- init.rb
+++ init.rb
@@ -5,6 +5,6 @@ Redmine::Plugin.register :redmine_per_project_sender do
   name 'Redmine per Project Sender plugin'
   author 'Christian A. Rodriguez'
   description 'Redmine per Project Sender plugin for overriding notification settings per project'
-  version '0.2.3'
+  version '0.3.1'
   requires_redmine :version_or_higher => '2.3.0'
 end
