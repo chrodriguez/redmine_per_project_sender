3ff5144ee01e16f69ba95b39b9c2a6e986ddda00
Anton Nechay <anton.nechay90@gmail.com>
2016-01-21 11:23:08 +0300

    Fix redmine 3.1-stable workaround

--- lib/mailer_patch.rb
+++ lib/mailer_patch.rb
@@ -13,19 +13,28 @@ module RedminePerProjectSender
       # Overrides the mail method trying to
       # get per project mail from configuration
       def mail_with_project_sender(headers={}, &block)
-        headers.merge! 'X-Mailer' => 'Redmine',
+        headers.reverse_merge! 'X-Mailer' => 'Redmine',
                 'X-Redmine-Host' => Setting.host_name,
                 'X-Redmine-Site' => Setting.app_title,
-                'X-Auto-Response-Suppress' => 'OOF',
+                'X-Auto-Response-Suppress' => 'All',
                 'Auto-Submitted' => 'auto-generated',
                 'From' => mail_from_project_sender,
                 'List-Id' => "<#{mail_from_project_sender.to_s.gsub('@', '.')}>"
 
+    # Replaces users with their email addresses
+    [:to, :cc, :bcc].each do |key|
+      if headers[key].present?
+        headers[key] = self.class.email_addresses(headers[key])
+      end
+    end
+
     # Removes the author from the recipients and cc
-    # if he doesn't want to receive notifications about what he does
+    # if the author does not want to receive notifications
+    # about what the author do
     if @author && @author.logged? && @author.pref.no_self_notified
-      headers[:to].delete(@author.mail) if headers[:to].is_a?(Array)
-      headers[:cc].delete(@author.mail) if headers[:cc].is_a?(Array)
+      addresses = @author.mails
+      headers[:to] -= addresses if headers[:to].is_a?(Array)
+      headers[:cc] -= addresses if headers[:cc].is_a?(Array)
     end
 
     if @author && @author.logged?
@@ -46,17 +55,17 @@ module RedminePerProjectSender
       headers[:references] = @references_objects.collect {|o| "<#{self.class.message_id_for(o)}>"}.join(' ')
     end
 
-    m= super headers do |format|
-      format.text
-      format.html unless Setting.plain_text_mail?
-    end
-
-    set_language_if_valid @initial_language
-
-
-        m
+    m = if block_given?
+      super headers, &block
+    else
+      super headers do |format|
+        format.text
+        format.html unless Setting.plain_text_mail?
       end
-
+    end
+    set_language_if_valid @initial_language
+    m
+end
       private
         # try to return mail_from project configuration
         def mail_from_project_sender
