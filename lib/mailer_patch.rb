b07bf2a0ce94b2bc5795177dee7163c8d8b258c0
Christian Rodriguez <chrodriguez@gmail.com>
2017-01-02 10:01:51 -0300

    Fix-001

--- lib/mailer_patch.rb
+++ lib/mailer_patch.rb
@@ -13,46 +13,57 @@ module RedminePerProjectSender
       # Overrides the mail method trying to
       # get per project mail from configuration
       def mail_with_project_sender(headers={}, &block)
-        headers.merge! 'X-Mailer' => 'Redmine',
-                'X-Redmine-Host' => Setting.host_name,
-                'X-Redmine-Site' => Setting.app_title,
-                'X-Auto-Response-Suppress' => 'OOF',
-                'Auto-Submitted' => 'auto-generated',
-                'From' => mail_from_project_sender,
-                'List-Id' => "<#{mail_from_project_sender.to_s.gsub('@', '.')}>"
+        headers.reverse_merge! 'X-Mailer' => 'Redmine',
+          'X-Redmine-Host' => Setting.host_name,
+          'X-Redmine-Site' => Setting.app_title,
+          'X-Auto-Response-Suppress' => 'All',
+          'Auto-Submitted' => 'auto-generated',
+          'From' => mail_from_project_sender,
+          'List-Id' => "<#{Setting.mail_from.to_s.gsub('@', '.')}>"
 
-    # Removes the author from the recipients and cc
-    # if he doesn't want to receive notifications about what he does
-    if @author && @author.logged? && @author.pref.no_self_notified
-      headers[:to].delete(@author.mail) if headers[:to].is_a?(Array)
-      headers[:cc].delete(@author.mail) if headers[:cc].is_a?(Array)
-    end
+        # Replaces users with their email addresses
+        [:to, :cc, :bcc].each do |key|
+          if headers[key].present?
+            headers[key] = self.class.email_addresses(headers[key])
+          end
+        end
 
-    if @author && @author.logged?
-      redmine_headers 'Sender' => @author.login
-    end
+        # Removes the author from the recipients and cc
+        # if the author does not want to receive notifications
+        # about what the author do
+        if @author && @author.logged? && @author.pref.no_self_notified
+          addresses = @author.mails
+          headers[:to] -= addresses if headers[:to].is_a?(Array)
+          headers[:cc] -= addresses if headers[:cc].is_a?(Array)
+        end
 
-    # Blind carbon copy recipients
-    if Setting.bcc_recipients?
-      headers[:bcc] = [headers[:to], headers[:cc]].flatten.uniq.reject(&:blank?)
-      headers[:to] = nil
-      headers[:cc] = nil
-    end
+        if @author && @author.logged?
+          redmine_headers 'Sender' => @author.login
+        end
 
-    if @message_id_object
-      headers[:message_id] = "<#{self.class.message_id_for(@message_id_object)}>"
-    end
-    if @references_objects
-      headers[:references] = @references_objects.collect {|o| "<#{self.class.message_id_for(o)}>"}.join(' ')
-    end
+        # Blind carbon copy recipients
+        if Setting.bcc_recipients?
+          headers[:bcc] = [headers[:to], headers[:cc]].flatten.uniq.reject(&:blank?)
+          headers[:to] = nil
+          headers[:cc] = nil
+        end
 
-    m= super headers do |format|
-      format.text
-      format.html unless Setting.plain_text_mail?
-    end
+        if @message_id_object
+          headers[:message_id] = "<#{self.class.message_id_for(@message_id_object)}>"
+        end
+        if @references_objects
+          headers[:references] = @references_objects.collect {|o| "<#{self.class.references_for(o)}>"}.join(' ')
+        end
 
-    set_language_if_valid @initial_language
-
+        m = if block_given?
+              super headers, &block
+            else
+              super headers do |format|
+                format.text
+                format.html unless Setting.plain_text_mail?
+              end
+            end
+        set_language_if_valid @initial_language
 
         m
       end
